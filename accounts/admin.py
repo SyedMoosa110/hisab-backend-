30771d8c213265d99a6eaae94fabce3c181d5202
Syed Moosa <syedmoosa.tms@gmail.com>
2026-07-09 14:32:18 +0500

    Initial backend commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Account, BackupRecord, Category, DuePayment, Note, Party, Transaction
+from .models import Account, BackupRecord, Category, DuePayment, Note, Party, Transaction, Stock, Sale
 
 
 admin.site.register(Account)
@@ -10,3 +10,5 @@ admin.site.register(Transaction)
 admin.site.register(DuePayment)
 admin.site.register(Note)
 admin.site.register(BackupRecord)
+admin.site.register(Stock)
+admin.site.register(Sale)
