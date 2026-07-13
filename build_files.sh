04b7f2418fb994dcfbf1047ebdb0a42cc2574b1b
Syed Moosa <syedmoosa.tms@gmail.com>
2026-07-13 14:43:33 +0500

    Initial backend commit

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-pip install -r requirements.txt --break-system-packages
+python3 -m venv build_env
+source build_env/bin/activate
+pip install -r requirements.txt
 python manage.py collectstatic --noinput --clear
 mkdir -p staticfiles_build/static
 cp -r staticfiles/. staticfiles_build/static/
